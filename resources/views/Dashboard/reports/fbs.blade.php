f0ba4e72e94b76bc1d5ba5d9088f6c41bf299d0d
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2018-03-04 12:19:45 +0530

    fixed fbs

--- resources/views/Dashboard/reports/fbs.blade.php
+++ resources/views/Dashboard/reports/fbs.blade.php
@@ -1,19 +1,19 @@
 <table class="weight table table-responsive">
-    <tr style="background-color: lightgrey;">
+    <tr style="height:70px;background-color: lightgrey;">
         <th style="font-weight: 500;">Description</th>
         <th style="font-weight: 500;">Result</th>
         <th style="font-weight: 500;">Unit</th>
         <th style="font-weight: 500;">Reference Range</th>
     </tr>
 
-        <tr>
+        <tr style="height: 70px">
             <td>F.B.S<br>(Fasting Blood Sugar)</td>
             <td>{{$det->fbs}}</td>
             <td>mg/dl</td>
             <td>75.0-115.0</td>
         </tr>
     @if($report->type=='5')
-        <tr>
+        <tr style="height: 70px;">
             <td>P.P.B.S<br>(Post Parandial Blood Sugar)</td>
             <td>{{$det->ppbs}}</td>
             <td>mg/dl</td>
@@ -21,13 +21,13 @@
         </tr>
     @endif
     @if($report->type=='6')
-    <tr>
+    <tr style="height: 70px;">
         <td>Urine Sugar</td>
         <td>{{$det->urine_sugar}}</td>
         <td></td>
         <td></td>
     </tr>
-        <tr>
+        <tr style="height: 70px;">
             <td>Urine Albumen</td>
             <td>{{$det->urine_albumen}}</td>
             <td></td>
