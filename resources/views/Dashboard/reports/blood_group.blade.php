98977c1f123bfb549c3da866042fd59d77eef815
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2018-03-04 11:35:16 +0530

    fixed blood group

--- resources/views/Dashboard/reports/blood_group.blade.php
+++ resources/views/Dashboard/reports/blood_group.blade.php
@@ -1,5 +1,7 @@
 <table class="weight table table-responsive">
     <tr style="background-color: lightgrey;">
+        <col width="40%">
+        <col width="60%">
         <th style="font-weight: 500;">Description</th>
         <th style="font-weight: 500;">Result</th>
     </tr>
