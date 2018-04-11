9471f93e4fa2882fabf06408bf741738b773be75
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2018-04-11 17:42:22 +0530

    Add type seeds

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -27,5 +27,45 @@ class UsersTableSeeder extends Seeder
         DB::table('type')->insert([
             'type'=>'BLOOD SUGAR SERIES TEST(3 Samples)'
         ]);
+        DB::table('type')->insert([
+            'type'=>'F.B.S'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'F.B.S'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'F.B.S & P.P.B.S'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'F.B.S & U.Sugar & Albumen'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'Urine Full Report (U.F.R)'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'Stool Full Report (S.F.R)'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'Pregnancy Test (H.C.G)'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'Pregnancy Test (H.C.G) & U.F.R'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'Blood Group'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'Lipid Profile'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'Serum Creatinine'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'Serum Creatinine & EGFR'
+        ]);
+        DB::table('type')->insert([
+            'type'=>'Serum Creatinine & Blood Urea'
+        ]);
+
     }
 }
